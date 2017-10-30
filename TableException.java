879614a8eab29218b49cb382ced68b146f56f347
Mario Del Real <mdelreal2@carthage.edu>
2017-10-30 17:12:25 -0500

    Changed comment to explain the TableException class' purpose

--- TableException.java
+++ TableException.java
@@ -1,6 +1,6 @@
 public class TableException extends Exception
 {
-	//class that exists to be thrown whenever the user requests to draw a number of cards that is greater than the number of cards in the deck
+	//class that exists to be thrown whenever the user requests to access the ArrayList of players at an invalid index
 	TableException(String message)
 	{
 		super(message);
